e1c79e8b4abc88ae904927e4f2647ecfb96b5539
Hemant Garg <hmtcool4u@gmail.com>
2018-07-29 03:50:52 +0530

    not so important

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,12 +2,15 @@ import React, { Component } from "react";
 import "./Home.css";
 
 import Main from '../Main/Main'
+import Initiatives from '../initiatives/initiatives';
+
 
 class Home extends Component {
   render() {
     return (
       <div className="Home">
         <Main />
+        <Initiatives />
       </div>
     )
   }
---
+++ src/components/initiatives/initiatives.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './initiatives.css';
+const Initiatives = () => {
+    return (
+        <div className="initiatives"></div>
+    );
+};
+
+export default Initiatives;
