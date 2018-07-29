c33bc7c855b15e78bd29493a0c80bfae403a19cd
Hemant Garg <hmtcool4u@gmail.com>
2018-07-29 07:54:39 +0530

    initial commit

--- src/components/initiatives/initiatives.js
+++ src/components/initiatives/initiatives.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import SVG1 from './svg1';
+import SVG2 from './svg2';
+import SVG3 from './svg3';
+
 import './initiatives.css';
+
+
 const Initiatives = () => {
     return (
-        <div className="initiatives"></div>
+        <section className="initiatives-section">
+        	<div className="heading">What We Do</div>
+        	<div className="initiatives">
+	        	<SVG1 />
+	        	<SVG2 />
+	        	<SVG3 />
+        	</div>
+        </section>
     );
 };
 
