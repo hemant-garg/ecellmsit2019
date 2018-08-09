9713f47582e4d548c94c5efde994dea44e194564
Hemant Garg <hmtcool4u@gmail.com>
2018-08-10 02:27:05 +0530

    navigation complete for mobile screens

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,6 +5,7 @@ import Logo from "./logo.png";
 
 class Header extends Component {
 	changeActiveClass = target => {
+		this.toggleNavForMobile();
 		const allNavs = target.parentNode.parentNode.childNodes;
 		allNavs.forEach(nav => nav.classList.remove("activeNav"));
 		target.parentNode.classList.add("activeNav");
@@ -17,6 +18,60 @@ class Header extends Component {
 		}, 4100);
 	}
 
+	renderList = cls => {
+		return (
+			<ul className={cls} id="header-navs">
+				<li
+					onClick={e => this.changeActiveClass(e.target)}
+					id="home"
+					className={`${cls}--item`}
+				>
+					<Link className={`LinkButton ${cls}--link`} to="/">
+						Home
+					</Link>
+				</li>
+				<li
+					onClick={e => this.changeActiveClass(e.target)}
+					id="gallery"
+					className={`${cls}--item`}
+				>
+					<Link className={`LinkButton ${cls}--link`} to="/gallery">
+						Gallery
+					</Link>
+				</li>
+				<li
+					onClick={e => this.changeActiveClass(e.target)}
+					className={`${cls}--item`}
+				>
+					<Link className={`LinkButton ${cls}--link`} to="/blogs">
+						Blogs
+					</Link>
+				</li>
+				<li
+					onClick={e => this.changeActiveClass(e.target)}
+					className={`${cls}--item`}
+				>
+					<a href="/#about-section" className={`${cls}--link`}>
+						About
+					</a>
+				</li>
+
+				<li onClick={this.showJoinUsMessage} className={`${cls}--item`}>
+					<button className={`LinkButton button-light ${cls}-join`}>
+						Join Us
+					</button>
+				</li>
+			</ul>
+		);
+	};
+
+	toggleNavForMobile() {
+		document
+			.getElementById("nav-ham-button")
+			.classList.toggle("toggleNavButton");
+		document.getElementById("nav-background").classList.toggle("show");
+	}
+
 	render() {
 		return (
 			<div className="header">
@@ -27,58 +82,13 @@ class Header extends Component {
 				<Link to="/" className="LinkButton">
 					<img className="header_logo" src={Logo} alt="" />
 				</Link>
-				<i class="fas fa-bars fa-3x nav_icon" />
-				<ul className="header_nav" id="header-navs">
-					<li
-						onClick={e => this.changeActiveClass(e.target)}
-						id="home"
-						className="header_nav--item"
-					>
-						<Link className=" LinkButton header_nav--link" to="/">
-							Home
-						</Link>
-					</li>
-					<li
-						onClick={e => this.changeActiveClass(e.target)}
-						id="gallery"
-						className="header_nav--item"
-					>
-						<Link
-							className=" LinkButton header_nav--link"
-							to="/gallery"
-						>
-							Gallery
-						</Link>
-					</li>
-					<li
-						onClick={e => this.changeActiveClass(e.target)}
-						className="header_nav--item"
-					>
-						<Link
-							className=" LinkButton header_nav--link"
-							to="/blogs"
-						>
-							Blogs
-						</Link>
-					</li>
-					<li
-						onClick={e => this.changeActiveClass(e.target)}
-						className="header_nav--item"
-					>
-						<a href="/#about-section" className="header_nav--link">
-							About
-						</a>
-					</li>
-
-					<li
-						onClick={this.showJoinUsMessage}
-						className="header_nav--item"
-					>
-						<button className="LinkButton button-light">
-							Join Us
-						</button>
-					</li>
-				</ul>
+				<div onClick={this.toggleNavForMobile} id="nav-ham-button" />
+				<div id="nav-background">
+					<div className="nav-mob">
+						{this.renderList("header_nav_mob")}
+					</div>
+				</div>
+				{this.renderList("header_nav")}
 			</div>
 		);
 	}
